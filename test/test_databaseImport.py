87adcfa2470d8ea48e91f1913eb1aa426eb91228
Gustavo Cayres <gustavocayrespublico@gmail.com>
2016-09-27 17:59:00 -0300

    Add first database tests.

--- test/test_databaseImport.py
+++ test/test_databaseImport.py
@@ -1,12 +1,25 @@
 from unittest import TestCase
+from source.db_modules.database_seed import DatabaseSeed
+from source.db_modules.database_import import DatabaseImport
 
 
 class TestDatabaseImport(TestCase):
     def setUp(self):
-        pass
+        db = DatabaseSeed('./test_db/test_db.sqlite')
+        db.drop_tables()
+        db.create_tables()
+        db.insert_organism("Test organism")
+        db.insert_chromosomes("./test_input/Test-organism_chromosome1.txt", 2, 2)
+        db.insert_replication_origins(10, "ToChr1")
+        db.insert_transcription_regions("./test_input/ToChr1_regions.txt", 1, 150)
+        db.close()
+
+        self.db = DatabaseImport('./test_db/test_db.sqlite')
 
     def test_import_origins_by_chromosome(self):
-        self.fail()
+        origins = self.db.import_origins_by_chromosome("ToChr1")
+        self.assertIsInstance(origins, list)
+        self.assertEqual(origins, [10])
 
     def test_import_regions_by_chromosome(self):
         self.fail()
@@ -16,3 +29,10 @@ class TestDatabaseImport(TestCase):
 
     def test_close(self):
         self.fail()
+
+    def tearDown(self):
+        db = DatabaseSeed('./test_db/test_db.sqlite')
+        db.drop_tables()
+        db.close()
+
+        self.db.close()
