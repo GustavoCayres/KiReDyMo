bc75d31f7cf146d83d3873ce8dfeea7f728ed471
Gustavo Cayres <gustavocayrespublico@gmail.com>
2016-10-25 16:59:01 -0200

    Remove output files from commit

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,6 @@ import subprocess
 
 from source.db_modules.database_insert import *
 from source.models.base_model import BaseModel
-
 BaseModel.set_database("tests/test_db/test_db.sqlite")
 from source.db_modules.database_create import *
 
