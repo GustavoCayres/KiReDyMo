4198eab139a2cb9a2ea6db2c5544578dbcc56078
Gustavo Cayres <gustavocayrespublico@gmail.com>
2017-02-16 18:01:26 -0200

    Separate output per chromosome instance

---
+++ format_output.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import re
+import os
+
+
+for file_name in os.listdir("output"):
+    regex = re.match("(TcChr([0-9]+)-S)_[0-9]", file_name)
+    if regex is not None:
+        output_path = "output/" + regex.group(1) + "_results.txt"
+        with open(output_path, 'a') as output_file:
+            if os.path.getsize(output_path) == 0:
+                print("[Simulation_Duration]\t"
+                      "[Head_Collision_Amount]\t[Tail_Collision_Amount]\t"
+                      "[Replication_Repair_Duration]\t[Transcription_Start_Delay]\t"
+                      "[Origins]\t", file=output_file)
+
+            file_path = "output/" + file_name
+            with open(file_path, 'r') as partial_file:
+                output_file.write(partial_file.read())
+
+        os.remove(file_path)
--- source/parameter_managers/origin_generation.py
+++ source/parameter_managers/origin_generation.py
@@ -18,7 +18,7 @@ def generate_origins(chromosome, replication_speed, replication_repair_duration)
     return origins
 
 
-def generate_randomized_origins(chromosome, replication_speed, replication_repair_duration):
+def generate_randomized_origins(chromosome, number_of_sets, replication_speed, replication_repair_duration):
     d = chromosome.length
     v = replication_speed
     ts = 8 * 3600  # duration of S phase
@@ -27,11 +27,14 @@ def generate_randomized_origins(chromosome, replication_speed, replication_repai
     random_number_generator = Random()
     random_number_generator.seed()
 
-    origins = []
-    for i in range(minimum_origin_amount):
-        position = random_number_generator.randrange(0, chromosome.length)
-        while position in [origin[0] for origin in origins]:
+    list_of_origin_sets = []
+    for i in range(number_of_sets):
+        origins_set = []
+        for j in range(minimum_origin_amount):
             position = random_number_generator.randrange(0, chromosome.length)
-        origins.append(ReplicationOrigin(position, 1, replication_speed, replication_repair_duration))
+            while position in [origin[0] for origin in origins_set]:
+                position = random_number_generator.randrange(0, chromosome.length)
+            origins_set.append(ReplicationOrigin(position, 1, replication_speed, replication_repair_duration))
+        list_of_origin_sets.append(origins_set)
 
-    return origins
+    return list_of_origin_sets
