2a8faca91131d7b7d4c8f1228e39c8b0be2e370e
Gustavo Cayres <gustavocayrespublico@gmail.com>
2017-02-17 15:40:28 -0200

    Substitute apply for map simulate function

--- KiReDyMo.py
+++ KiReDyMo.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
+import copy
 import errno
 import os
 import sys
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
 
 from source.database_management.database import Database
 from source.parameter_managers.origin_generation import *
 from source.simulation_modules.simulation import Simulation
 
 
-def simulate(chromosome, simulation_number):
+def simulate(args):
+    chromosome = args[0]
+    simulation_number = args[1]
+
     with open("output/" + chromosome.code + "_" + str(simulation_number) + "_results.txt", 'w') as output_file:
         simulation = Simulation(chromosome)
         simulation_duration, head_collisions, tail_collisions, repair_duration,\
@@ -41,19 +45,20 @@ def main(args):
         if exception.errno != errno.EEXIST:
             raise
 
-    cores = Pool(cpu_count())
+    cores = Pool()
+    chromosomes = []
     for chromosome in parse_arguments(args[1]):
         simulation_counter = 1
         for replication_origins in generate_randomized_origins(chromosome, int(args[2]), 67, 0):
             for replication_repair_duration in range(0, 28800, 11520):
                 for transcription_start_delay in range(10, 2000, 700):
-                    chromosome.update_attributes(transcription_start_delay=transcription_start_delay,
-                                                 replication_repair_duration=replication_repair_duration,
-                                                 replication_origins=replication_origins)
-                    print(simulation_counter)
-                    cores.apply_async(simulate, (chromosome, simulation_counter))
+                    chromosome_copy = copy.deepcopy(chromosome)
+                    chromosome_copy.update_attributes(transcription_start_delay=transcription_start_delay,
+                                                      replication_repair_duration=replication_repair_duration,
+                                                      replication_origins=replication_origins)
+                    chromosomes.append((chromosome_copy, simulation_counter))
                     simulation_counter += 1
-
+    cores.map(simulate, chromosomes)
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Run with: ./KiReDyMo <file_with_parameters>", file=sys.stderr)
