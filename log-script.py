45137a5bb03518411bd1da21689898173a6a0619
Gustavo Rodrigues Cayres <gustavocayrespublico@gmail.com>
2017-09-22 18:11:16 -0300

    Create script to gather logs

---
+++ log-script.py
@@ -0,0 +1,38 @@
+import os
+
+
+output_path = "./output/"
+joined_logs_path = "./joined_logs/"
+for simulation_folder_name in next(os.walk(output_path))[1]:
+    simulation_path = output_path + simulation_folder_name + "/"
+    os.makedirs(joined_logs_path, exist_ok=True)
+    os.makedirs(joined_logs_path + "valid/", exist_ok=True)
+    os.makedirs(joined_logs_path + "not_valid/", exist_ok=True)
+    valid = True
+    for result_file_name in next(os.walk(simulation_path))[2]:
+        result_path = simulation_path + result_file_name
+        with open(result_path) as result_file:
+            result_file.readline()
+            if result_file.readline().split('\t')[0] == '10000':
+                valid = False
+
+    final_path = joined_logs_path + "valid/" if valid else joined_logs_path + "not_valid/"
+    with open(final_path + simulation_folder_name + ".txt", 'w') as joined_logs_file:
+        for log_folder in next(os.walk(simulation_path))[1]:
+            log_path = simulation_path + log_folder + "/"
+            eteds = []
+            for log_file_name in next(os.walk(log_path))[2]:
+                file_path = log_path + log_file_name
+                with open(file_path) as log_file:
+                    origins = []
+                    log_file.readline()
+                    for line in log_file:
+                        origins.append(int(line.split('\t')[1]))
+
+                    origins.sort()
+                    eted_start = 0
+                    for origin in origins:
+                        eteds.append(str(origin - eted_start) + '\n')
+                        eted_start = origin
+
+            joined_logs_file.writelines(eteds)
