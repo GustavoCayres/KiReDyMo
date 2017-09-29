de99ecbf04b973651e27826d35fe340bc05006f7
Gustavo Rodrigues Cayres <gustavocayrespublico@gmail.com>
2017-09-29 17:06:04 -0300

    Update log script

--- log-script.py
+++ log-script.py
@@ -13,7 +13,7 @@ for simulation_folder_name in next(os.walk(output_path))[1]:
         result_path = simulation_path + result_file_name
         with open(result_path) as result_file:
             result_file.readline()
-            if result_file.readline().split('\t')[0] == '10000':
+            if int(result_file.readline().split('\t')[0]) > 7080 and abs(int(result_file.readline().split('\t')[2]) - 260000) > 260000*.1:
                 valid = False
 
     final_path = joined_logs_path + "valid/" if valid else joined_logs_path + "not_valid/"
