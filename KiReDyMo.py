2cd9e2f1de8ecc2e0394cdb5b56a16cf172ca49c
Marcelo Reis <marcelo.reis@gmail.com>
2017-10-24 11:38:50 -0200

    Inclusion of GNU GPL license v3

--- KiReDyMo.py
+++ KiReDyMo.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+
+#
+# KiReDyMo.py: main program of the KiReDyMo program.
+#
+#    This file is part of the KiReDyMo program.
+#
+#    Copyright (C) 2017 Gustavo Rodrigues Cayres-Silva and Marcelo S. Reis.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+
+
 import sys
 import time
 
